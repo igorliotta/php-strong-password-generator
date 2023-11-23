60e2f0e252ab7790e9affed4dfb32e16ccfae0d9
igorliotta <liottaigor@gmail.com>
2023-11-23 17:55:00 +0100

    Fix:

--- welcome/welcome.php
+++ welcome/welcome.php
@@ -23,7 +23,7 @@ $password = $_SESSION['password'] ?? '';
 <body>
     <div class="container container my-5 py-5">
         <p class="my-5">Ecco qui la tua password generata: <span class="bg-danger text-white py-2 px-4"><?php echo $password; ?></span></p>
-        <p class="my-5">La password è lunga <?php echo strlen($password); ?> caratteri!</p>
+        <p class="my-5">La password è lunga <?php echo strlen($password); ?> caratteri come richiesto!</p>
         <form action="../index.php">
             <input type="submit" value="Indietro">
         </form>
